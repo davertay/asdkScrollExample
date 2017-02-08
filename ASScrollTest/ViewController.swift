dd77b1cabf7b7f509672de29edb939553ea5ab75
David Taylor <dtaylor@groupon.com>
2017-02-08 14:53:57 -0800

    Added some more failing experiments

--- ASScrollTest/ViewController.swift
+++ ASScrollTest/ViewController.swift
@@ -9,6 +9,27 @@
 import UIKit
 import AsyncDisplayKit
 
+func makeABunchOfNodes() -> [ASLayoutElement] {
+    return [
+        FlagNode(descr: "red", color: UIColor.red, height: 80),
+        FlagNode(descr: "yellow", color: UIColor.yellow, height: 40),
+        FlagNode(descr: "blue", color: UIColor.blue, height: 120),
+        FlagNode(descr: "green", color: UIColor.green, height: 300),
+        FlagNode(descr: "orange", color: UIColor.orange, height: 10),
+        FlagNode(descr: "black", color: UIColor.black, height: 160),
+        FlagNode(descr: "cyan", color: UIColor.cyan, height: 100),
+    ]
+}
+
+func configureScrollNode(_ scrollNode: ASScrollNode, automaticallyManagesContentSize: Bool) {
+    scrollNode.automaticallyManagesSubnodes = true
+    scrollNode.automaticallyManagesContentSize = automaticallyManagesContentSize
+    scrollNode.backgroundColor = UIColor.lightGray
+    scrollNode.style.width = ASDimensionMake(ASDimensionUnit.fraction, 1.0)
+}
+
+
+
 class FlagNode: ASDisplayNode {
     let descr: String
     let height: CGFloat
@@ -30,23 +51,18 @@ class FlagNode: ASDisplayNode {
     }
 }
 
+
+
 class ContentNode: ASDisplayNode {
     let verticalNodes: [ASLayoutElement]
 
     override init() {
-        verticalNodes = [
-            FlagNode(descr: "red", color: UIColor.red, height: 80),
-            FlagNode(descr: "yellow", color: UIColor.yellow, height: 40),
-            FlagNode(descr: "blue", color: UIColor.blue, height: 120),
-            FlagNode(descr: "green", color: UIColor.green, height: 300),
-            FlagNode(descr: "orange", color: UIColor.orange, height: 10),
-            FlagNode(descr: "black", color: UIColor.black, height: 160),
-            FlagNode(descr: "cyan", color: UIColor.cyan, height: 100),
-        ]
+        verticalNodes = makeABunchOfNodes()
         super.init()
         automaticallyManagesSubnodes = true
         backgroundColor = UIColor.white
         style.width = ASDimensionMake(ASDimensionUnit.fraction, 1.0)
+        // style.height = ASDimensionMake(ASDimensionUnit.points, CGFloat.greatestFiniteMagnitude)
     }
 
     override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
@@ -74,26 +90,109 @@ class ContentNode: ASDisplayNode {
     }
 }
 
+
+
+class ContentScrollNode: ASScrollNode {
+    let verticalNodes: [ASLayoutElement]
+
+    override init() {
+        verticalNodes = makeABunchOfNodes()
+        super.init()
+        configureScrollNode(self, automaticallyManagesContentSize: true)
+    }
+
+    // FIXME: initialiser chain is broken so we get initialized twice
+    // we have to implement this or we'll crash
+    override init(viewBlock: @escaping ASDisplayNodeViewBlock, didLoad didLoadBlock: ASDisplayNodeDidLoadBlock? = nil) {
+        verticalNodes = makeABunchOfNodes()
+        super.init(viewBlock: viewBlock, didLoad: didLoadBlock)
+        configureScrollNode(self, automaticallyManagesContentSize: true)
+    }
+
+    // Does not work
+    override func layoutSpecThatFits(_ constrainedSize: ASSizeRange) -> ASLayoutSpec {
+        let verticalStack = ASStackLayoutSpec.vertical()
+        verticalStack.children = verticalNodes
+        return verticalStack
+    }
+}
+
+
+
 class ViewController: ASViewController<ASScrollNode> {
-    let autoContentSize = true
+    var autoContentSize = true
     let contentNode: ContentNode
 
     required init?(coder aDecoder: NSCoder) {
         let contentNode = ContentNode()
         self.contentNode = contentNode
 
+        /** UNCOMMENT ONLY ONE OF THESE EXPERIMENTS **/
+
+        // EXPERIMENT 1: ASScrollNode subclassed directly that has an ASStackLayoutSpec:
+        // Does not work
+        let scrollNode = ContentScrollNode()
+
+        // EXPERIMENT 2: ASScrollNode using layoutSpecBlock instead of subclassing to return an ASStackLayoutSpec:
+        // Does not work
+        // let scrollNode = ViewController.scrollNodeWithVerticalLayout(automaticallyManagesContentSize: autoContentSize)
+
+        // EXPERIMENT 3: ASScrollNode wrapping an ASDisplayNode that has an ASStackLayoutSpec wrapped in a ASWrapperLayoutSpec:
+        // Does not work
+        // let scrollNode = ViewController.scrollNodeWrappingContentNode(contentNode, automaticallyManagesContentSize: autoContentSize)
+
+        // EXPERIMENT 4: ASScrollNode wrapping an ASDisplayNode that has an ASStackLayoutSpec wrapped in a ASWrapperLayoutSpec and has the node's style dimensions set to the exact desired content size:
+        // Does not work when using automaticallyManagesContentSize = true
+        // Set autoContentSize to false to get this to work using the manual layout contentSize hacks
+        // autoContentSize = false
+        // let scrollNode = ViewController.scrollNodeWrappingContentNodeWithForcedLayout(contentNode, automaticallyManagesContentSize: autoContentSize)
+
+        super.init(node: scrollNode)
+    }
+
+    // Attempt to provide an unbounded height for the layout calculations
+    // Note that this doen't help - it gets trumped by the bounds during the viewWillAppear layout pass
+    override func nodeConstrainedSize() -> ASSizeRange {
+        if autoContentSize {
+            let width: CGFloat = isViewLoaded ? view.bounds.width : 320
+            let size = CGSize(width: width, height: 100000) // crashes if you use CGFloat.greatestFiniteMagnitude!!
+            return ASSizeRangeMake(size)
+        } else {
+            return super.nodeConstrainedSize()
+        }
+    }
+
+    static func scrollNodeWithVerticalLayout(automaticallyManagesContentSize: Bool) -> ASScrollNode {
         let scrollNode = ASScrollNode()
-        scrollNode.automaticallyManagesSubnodes = true
-        scrollNode.automaticallyManagesContentSize = autoContentSize
-        scrollNode.backgroundColor = UIColor.lightGray
-        scrollNode.style.width = ASDimensionMake(ASDimensionUnit.fraction, 1.0)
+        let verticalNodes = makeABunchOfNodes()
+        configureScrollNode(scrollNode, automaticallyManagesContentSize: automaticallyManagesContentSize)
+        scrollNode.layoutSpecBlock = { (node, constrainedSize) in
+            let verticalStack = ASStackLayoutSpec.vertical()
+            verticalStack.children = verticalNodes
+            return verticalStack
+        }
+        return scrollNode
+    }
+
+    static func scrollNodeWrappingContentNode(_ contentNode: ContentNode, automaticallyManagesContentSize: Bool) -> ASScrollNode {
+        let scrollNode = ASScrollNode()
+        configureScrollNode(scrollNode, automaticallyManagesContentSize: automaticallyManagesContentSize)
+        scrollNode.layoutSpecBlock = { (node, constrainedSize) in
+            return ASWrapperLayoutSpec(layoutElement: contentNode)
+        }
+        return scrollNode
+    }
+
+    static func scrollNodeWrappingContentNodeWithForcedLayout(_ contentNode: ContentNode, automaticallyManagesContentSize: Bool) -> ASScrollNode {
+        let scrollNode = ASScrollNode()
+        configureScrollNode(scrollNode, automaticallyManagesContentSize: automaticallyManagesContentSize)
         scrollNode.layoutSpecBlock = { (node, constrainedSize) in
             let contentSize = contentNode.calculatePreferredLayoutSize(constrainedSize: constrainedSize)
             contentNode.forceStyleSize(size: contentSize)
             contentNode.style.layoutPosition = CGPoint(x: 0, y: 0)
-            return ASAbsoluteLayoutSpec(sizing: .default, children: [contentNode])
+            return ASWrapperLayoutSpec(layoutElement: contentNode)
         }
-        super.init(node: scrollNode)
+        return scrollNode
     }
 
     override func viewDidLayoutSubviews() {
